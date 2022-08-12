5268bb0eb4f7c8fde774373ccd7f40e63a613db2
AslanHasanov <aslanrh@code.edu.az>
2022-08-12 20:37:34 +0400

    Created UserValidation class in Validation folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Validations/UserValidation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentFinalProject.ApplicationLogic.Validations
+{
+    internal class UserValidation
+    {
+
+    }
+}
