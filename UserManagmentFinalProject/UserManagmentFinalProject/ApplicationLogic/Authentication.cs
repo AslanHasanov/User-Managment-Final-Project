b844fd5bedff6f56cef96a7a8cec9ef6d9fc7587
AslanHasanov <aslanrh@code.edu.az>
2022-08-12 20:58:13 +0400

    GetFirstName and GetLastName methods added to Authentication class

--- UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Authentication.cs
+++ UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Authentication.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagmentFinalProject.ApplicationLogic.Validations;
 
 namespace UserManagmentFinalProject.ApplicationLogic
 {
     public class Authentication
     {
+        private static string GetFirstName()
+        {
+            string firstName = string.Empty;
+            bool isExceptionExists = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter your first name :");
+                    firstName = Console.ReadLine();
+                    isExceptionExists = false;
+                }
+                catch
+                {
+                    Console.Write("Something went wrong :");
+                    isExceptionExists = true;
+                }
+            } while (!isExceptionExists && !UserValidation.IsValidFirstName(firstName));
+
+
+            return firstName;
+        }
+
+        private static string GetLastName()
+        {
+            string lastName = string.Empty;
+            bool isExceptionExists = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter your last name :");
+                    lastName = Console.ReadLine();
+                    isExceptionExists = false;
+                }
+                catch
+                {
+                    Console.Write("Something went wrong :");
+                    isExceptionExists = true;
+                }
+            } while (!isExceptionExists && !UserValidation.IsValidLastName(lastName));
+
+
+            return lastName;
+        }
     }
 }
--- UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Dashboard.cs
+++ UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Dashboard.cs
@@ -8,5 +8,6 @@ namespace UserManagmentFinalProject.ApplicationLogic
 {
     public class Dashboard
     {
+
     }
 }
