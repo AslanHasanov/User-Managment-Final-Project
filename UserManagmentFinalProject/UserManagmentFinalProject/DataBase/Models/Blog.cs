3143a216b18c65803ae05c970aa11c49ba3190f4
AslanHasanov <aslanrh@code.edu.az>
2022-08-12 21:15:42 +0400

    Created Blog class in Models folder and Created BlogRepo class in Repo folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Models/Blog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models.Common;
+using UserManagmentFinalProject.DataBase.Models.Enums;
+using UserManagmentFinalProject.DataBase.Repo;
+
+namespace UserManagmentFinalProject.DataBase.Models
+{
+    public class Blog : Entity<string>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public BlogStatus Status { get; set; }
+        public User FromUser { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public Blog(User fromUser, string title, string content, BlogStatus status, string id = null)
+        {
+            Title = title;
+            Content = content;
+            Status = status;
+            FromUser = fromUser;
+            if (id != null)
+            {
+                Id = id;
+            }
+            else
+            {
+                Id = BlogRepo.IdBlog;
+            }
+        }
+        public string GetBlogInfo()
+        {
+            return $" {CreatedOn.ToString("dd/MM/yyyy")}  {Id} \n {Title} \n {Content} \n {Status}";
+        }
+    }
+}
---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Repo/BlogRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models;
+using UserManagmentFinalProject.DataBase.Models.Enums;
+using UserManagmentFinalProject.DataBase.Repo.Common;
+
+namespace UserManagmentFinalProject.DataBase.Repo
+{
+    public class BlogRepo : Repository<Blog, string>
+    {
+        static Random random = new Random();
+        private static string _idCounter;
+        public static string IdBlog
+        {
+            get
+            {
+                _idCounter = "BL" + random.Next();
+                return _idCounter;
+            }
+        }
+        public static Blog Add(User fromUser, string title, string content)
+        {
+            Blog blog = new Blog(fromUser, title, content, BlogStatus.Sended);
+            DbContext.Add(blog);
+            return blog;
+        }
+    }
+}
