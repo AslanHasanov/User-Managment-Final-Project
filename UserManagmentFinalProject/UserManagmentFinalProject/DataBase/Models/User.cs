8b4252d2198ee95d2096a6a0b6692a7c33c01c5a
AslanHasanov <aslanrh@code.edu.az>
2022-08-12 20:00:16 +0400

    Created User class in Models folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentFinalProject.DataBase.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public User(string firstName, string lastName, string email, string password, int? id = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+
+            
+        }
+        public virtual string GetInfo()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
