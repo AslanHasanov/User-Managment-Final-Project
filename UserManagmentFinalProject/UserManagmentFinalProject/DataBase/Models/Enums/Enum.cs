90c8fdcaf10fe2072cc883e0a20067c60eb7070d
AslanHasanov <aslanrh@code.edu.az>
2022-08-12 21:12:02 +0400

    Created Enum in Enums folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Models/Enums/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentFinalProject.DataBase.Models.Enums
+{
+    public enum BlogStatus
+    {
+        Sended = 1,
+        Approved = 2,
+        Rejected = 3,
+    }
+}
