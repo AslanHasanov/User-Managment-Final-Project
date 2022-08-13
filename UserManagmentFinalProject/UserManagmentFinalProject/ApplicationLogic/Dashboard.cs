4c079fb8adbee29c0f75313b9757f31a1ccf3725
AslanHasanov <aslanrh@code.edu.az>
2022-08-13 16:18:21 +0400

    Created and initialized AdminPanel method in Dashboard class

--- UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Dashboard.cs
+++ UserManagmentFinalProject/UserManagmentFinalProject/ApplicationLogic/Dashboard.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models;
+using UserManagmentFinalProject.DataBase.Repo;
 
 namespace UserManagmentFinalProject.ApplicationLogic
 {
-    public class Dashboard
+    public partial class Dashboard
     {
+        public static User CurrentUser { get; set; }
+        public static void AdminPanel()
+        {
+            BlogRepo blogRepo = new BlogRepo();
+            InboxRepo inboxRepo = new InboxRepo();
+            UserRepo userRepo = new UserRepo();
+            Admin admin = (Admin)UserRepo.GetUserByEmail(CurrentUser.Email);
 
+            Console.WriteLine($"Welcome dear admin, {admin.GetInfo()}");
+
+            Console.Write("Admin's commands :" +
+                "\n /show-users " +
+                "\n /show-admins " +
+                "\n /show-auditing-blogs " +
+                "\n /approve-blog " +
+                "\n /reject-blog " +
+                "\n /log-out");
+        }
     }
 }
