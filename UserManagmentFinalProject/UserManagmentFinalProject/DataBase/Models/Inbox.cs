0c5fd0ae5cf4765187ea3ce4a7e94344ff2c0524
AslanHasanov <aslanrh@code.edu.az>
2022-08-13 15:57:12 +0400

    Created and initialized Inbox class in Models folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Models/Inbox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models.Common;
+using UserManagmentFinalProject.DataBase.Repo;
+
+namespace UserManagmentFinalProject.DataBase.Models
+{
+    public class Inbox : Entity<int>
+    {
+        public string Notification { get; set; }
+
+        public User User { get; set; }
+
+        public Inbox(string notification, User user, int? id = null)
+        {
+            Notification = notification;
+            User = user;
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else
+            {
+                Id = UserRepo.IdCounter;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Notification}";
+        }
+    }
+}
