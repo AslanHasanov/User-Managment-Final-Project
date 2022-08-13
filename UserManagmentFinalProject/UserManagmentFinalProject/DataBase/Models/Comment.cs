083a628e4f389121381e47947d8edfb4a67ec035
AslanHasanov <aslanrh@code.edu.az>
2022-08-13 15:53:17 +0400

    Created and initialize Comment class in Models folder, Created and initialize CommentRepo class in Repo folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Models/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models.Common;
+using UserManagmentFinalProject.DataBase.Repo;
+
+namespace UserManagmentFinalProject.DataBase.Models
+{
+    public class Comment : Entity<int>
+    {
+        public Blog Blog { get; set; }
+        public User FromUser { get; set; }
+        public string ContentComment { get; set; }
+
+        public Comment(Blog blog, User fromUser, string contentComment, int? id = null)
+        {
+            Blog = blog;
+            FromUser = fromUser;
+            ContentComment = contentComment;
+
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else
+            {
+                Id = CommentRepo.IdComment;
+            }
+
+        }
+        public string GetInfo()
+        {
+            return $" {CreatedAt} {FromUser.FirstName} {FromUser.LastName} {ContentComment}";
+        }
+    }
+}
---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Repo/CommentRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models;
+using UserManagmentFinalProject.DataBase.Repo.Common;
+
+namespace UserManagmentFinalProject.DataBase.Repo
+{
+    public class CommentRepo : Repository<Comment, int>
+    {
+
+        private static int _idCounter;
+        public static int IdComment
+        {
+            get
+            {
+                _idCounter++;
+                return _idCounter;
+            }
+        }
+
+        public static Comment AddComment(User fromUser, Blog blog, string content)
+        {
+            Comment comment = new Comment(blog, fromUser, content);
+            DbContext.Add(comment);
+            return comment;
+        }
+        public static BlogRepo BlogRepo = new BlogRepo();
+
+        static CommentRepo()
+        {
+            DbContext.Add(new Comment(BlogRepo.GetById("yehya"), UserRepo.GetUserByEmail("yehya@gmail.com"), "tebrikler"));
+
+        }
+    }
+}
