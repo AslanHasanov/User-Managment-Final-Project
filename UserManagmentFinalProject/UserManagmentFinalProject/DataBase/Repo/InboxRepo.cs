9078afd6053c5ccdbe6048f312be33d2f7a40867
AslanHasanov <aslanrh@code.edu.az>
2022-08-13 15:58:42 +0400

    Created InboxRepo class in Repo folder

---
+++ UserManagmentFinalProject/UserManagmentFinalProject/DataBase/Repo/InboxRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentFinalProject.DataBase.Models;
+using UserManagmentFinalProject.DataBase.Repo.Common;
+
+namespace UserManagmentFinalProject.DataBase.Repo
+{
+    public class InboxRepo : Repository<Inbox, int>
+    {
+
+        static InboxRepo()
+        {
+            DbContext.Add(new Inbox("Salam dunya", UserRepo.GetUserByEmail("eshqin@gmail.com")));
+        }
+    }
+}
